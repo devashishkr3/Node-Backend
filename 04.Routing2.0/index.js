3dd0a18af47f07d7532b4787eb7702f80cb945c6
Devashish Kumar <dk3127197@gmail.com>
2026-06-25 21:12:10 +0530

    Routing 2.0

---
+++ 04.Routing2.0/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/hello", (req,res) =>{
+    res.send("Hello");
+})
+
+app.get("/search", (req, res) =>{
+    let {name} = req.query
+    console.log(req.query);
+    res.send(`you searched for ${name}`)
+})
+
+app.get("/:username", (req, res) =>{
+    let {username} = req.params;
+    console.log(username);
+
+    // find for that username on database
+
+
+
+    res.send(`Hello ${username}`);
+})
+
+
+
+app.get("/" , (req, res) =>{
+    res.send("Hello From Home");
+})
+
+app.use((req, res) =>{
+    // console.log("Incoming Request");
+    res.send(`
+        <div style="
+        text-align:center;
+        color:white;
+    ">
+
+        <h1 style="
+            font-size:120px;
+            margin:0;
+            color:#38bdf8;
+        ">
+            404
+        </h1>
+
+        <h2 style="
+            margin:10px 0;
+            font-size:35px;
+            color:red;
+        ">
+            Page Not Found
+        </h2>
+
+        <p style="
+            color:#cbd5e1;
+            font-size:18px;
+            margin-bottom:30px;
+        ">
+            Oops! The page you're looking for doesn't exist.
+        </p>
+
+        <a href="/"
+           style="
+           text-decoration:none;
+           background:#38bdf8;
+           color:white;
+           padding:12px 25px;
+           border-radius:8px;
+           font-size:16px;
+           font-weight:bold;
+        ">
+            Go Back Home
+        </a>
+
+    </div>
+        `);
+})
+
+app.listen(8080, () =>{
+    console.log("Server is running on port 8080");
+})
