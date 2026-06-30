79d145bc2e5fc1e745cb8df2232b13fdafbef3d8
Devashish Kumar <dk3127197@gmail.com>
2026-06-30 22:05:20 +0530

    EJS

---
+++ 06.static_file/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+app.get("/", (req, res) =>{
+    res.render("home.ejs");
+})
+
+
+
+app.listen(PORT, () =>{
+    console.log(`server is running on PORT ${PORT}`);
+})
