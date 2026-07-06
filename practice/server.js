1d89906139618c6f39620befa6bf71703af757fa
Devashish Kumar <dk3127197@gmail.com>
2026-07-06 14:05:00 +0530

    practice file

---
+++ practice/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.render("home.ejs");
+})
+
+
+
+app.listen(PORT, () =>{
+    console.log(`server is running on port ${PORT}`)
+})
