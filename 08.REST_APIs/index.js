f16d3bacfecf7f60f879c4bea380cadf9f1f641e
Devashish Kumar <dk3127197@gmail.com>
2026-07-07 22:13:46 +0530

    UUID/CUID, edit form and patch method with method override npm package

--- 08.REST_APIs/index.js
+++ 08.REST_APIs/index.js
@@ -23,6 +23,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8080;
+const cuid = require('cuid');
+const methodOverride = require('method-override');
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -33,26 +36,33 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use(methodOverride("_method"));
+
 let posts = [
     {
         username: "baba01",
         content: "This is my first post",
+        id: "10001",
     },
     {
         username: "sachin",
         content: "I love cricket",
+        id: "10002",
     },
     {
         username: "sourav",
         content: "game is not over yet",
+        id: "10003",
     },
     {
         username: "tanya",
         content: "I Love coding",
+        id: "10004",
     },
     {
         username: "devashish",
         content: "I am a full stack developer",
+        id: "10005",
     },
     
 ]
@@ -71,19 +81,40 @@ app.get("/posts/new", (req, res) =>{
 
 app.post("/posts", (req, res) =>{
     let {username, content} = req.body;
+
+    let id = cuid();
     
-    posts.push({username, content});
+    posts.push({username, content, id});
     res.redirect("/posts");
 })
 
-app.get("/posts/:username", (req, res) =>{
-    let {username} = req.params;
-    let post = posts.find((p) => username === p.username);
+app.get("/posts/:id", (req, res) =>{
+    let {id} = req.params;
+    // console.log(id);
+    let post = posts.find((p) => p.id === id);
+    // console.log(post);
     if(post){
         res.render("show.ejs", {post});
     }else{
         res.send("Post not found.");
     }
+})
+
+app.patch("/posts/:id", (req, res) =>{
+    let {id} = req.params;
+    let {content} = req.body;
+
+    let post = posts.find((p) => p.id === id);
+
+    post.content = content;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit", (req, res) =>{
+    let {id} = req.params;
+    let post = posts.find((p) => p.id === id);
+
+    res.render("edit.ejs", {post});
 })
 
 
