14e35bbb7a41a70ae848f7d2575621b75b2e7a21
Devashish Kumar <dk3127197@gmail.com>
2026-07-09 09:51:28 +0530

    delete method

--- 08.REST_APIs/index.js
+++ 08.REST_APIs/index.js
@@ -117,6 +117,13 @@ app.get("/posts/:id/edit", (req, res) =>{
     res.render("edit.ejs", {post});
 })
 
+app.delete("/posts/:id", (req, res) =>{
+    let {id} = req.params;
+
+    posts = posts.filter((p) => p.id !== id);
+
+    res.redirect("/posts");
+})
 
 
 
--- 08.REST_APIs/public/script.js
+++
@@ -1,3 +0,0 @@
-for(let i=10;i<=100;i++){
-    ksjfhdks
-}
