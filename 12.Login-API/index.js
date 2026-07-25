2ff3c88aaa0b34eee8703b6a5ea3b5da590a4961
Devashish Kumar <dk3127197@gmail.com>
2026-07-25 21:16:36 +0530

    update and login api fixed

--- 12.Login-API/index.js
+++ 12.Login-API/index.js
@@ -2,69 +2,85 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const mysql = require("mysql2");
-require('dotenv').config();
+require("dotenv").config();
 const cuid = require("cuid");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const connection = mysql.createConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
 });
 
 // api for signup/register
-app.post("/signup", (req, res) =>{
-    const {name, email, password, dob} = req.body;
-    let id = cuid();
-    let registerQuery = 'INSERT INTO user(id, name, email, password, dob) VALUES(?,?,?,?,?)';
-    let userArr = [id, name, email, password, dob];
+app.post("/signup", (req, res) => {
+  const { name, email, password, dob } = req.body;
+  let id = cuid();
+  let registerQuery =
+    "INSERT INTO user(id, name, email, password, dob) VALUES(?,?,?,?,?)";
+  let userArr = [id, name, email, password, dob];
 
-    try {
-        connection.query(registerQuery, userArr, (err, result) =>{
-        if(err) throw err;
-        console.log(result);
-    })
+  try {
+    connection.query(registerQuery, userArr, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
 
     res.status(201).json({
-        message : "User Registered Successfully.",
-        success: true,
-    })
-
-
-    } catch (error) {
-        return res.status(500).json({
-            error: "Internal server error",
-            success: false,
-        })
-    }
-})
-
-// app.post("/login", (req, res) =>{
-//     const {email, password} = req.body;
-    
-//     let loginQuery = `SELECT * FROM user WHERE email=?`;
-//     let existUserArr;
-
-//     connection.query(loginQuery,email, (err, result)=>{
-//         if(err) throw err;
-//         console.log(result);
-//         existUserArr = result;
-//     })
-//     let existUser = existUserArr[0];
-//     if(existUser.password == password){
-//         return res.status(200).json({
-//             message: "Login Successful.",
-//             success: true
-//         })
-//     }
-// })
-
-// api for login - POST method 
-
-app.listen(PORT, () =>{
-    console.log(`Server is running on PORT ${PORT}`);
+      message: "User Registered Successfully.",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "Internal server error",
+      success: false,
+    });
+  }
 });
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  let loginQuery = `SELECT * FROM user WHERE email=?`;
+  // let existUserArr = [];
+
+  connection.query(loginQuery, email, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        error: err,
+        success: false,
+      });
+    }
+
+    if (result.length == 0 || !result) {
+      return res.status(404).json({
+        message: "User not found with this email Please Login first.",
+        success: true,
+      });
+    }
+
+    let existUser = result[0];
+    if (existUser.password == password) {
+      return res.status(200).json({
+        message: "Login Successful.",
+        success: true,
+      });
+    }
+
+    if (existUser.password != password) {
+      return res.status(400).json({
+        message: "Invalid Credentials.",
+        success: false,
+      });
+    }
+  });
+});
+
+// api for login - POST method
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
